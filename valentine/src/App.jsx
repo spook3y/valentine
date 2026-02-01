92beeca1c7ef8a5e102b4fc399a0383f2402b568
Carter Cross <cartercross1@gmail.com>
2026-02-01 12:22:29 -0600

    fixed video

--- valentine/src/App.jsx
+++ valentine/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         // Do not use 'autoplay' or 'muted' here initially
         style={{ display: hasInteracted ? "block" : "none" }}
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div
